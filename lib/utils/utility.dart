9b557a232003be9db83285024af6d86af4478bde
Shahil <shahil1.jha@gmail.com>
2021-09-03 12:49:42 +0545

    func: generate password

--- lib/utils/utility.dart
+++ lib/utils/utility.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'dart:convert';
+import 'dart:math';
 
 import 'package:crypto/crypto.dart';
 import 'package:encrypt/encrypt.dart' as encrypt;
@@ -121,5 +122,27 @@ class Utility {
     final String decrypted =
         encrypter.decrypt(encrypt.Encrypted.fromBase64(str), iv: iv);
     return decrypted;
+  }
+
+  static String generatePassword({
+    bool letter = true,
+    bool isNumber = true,
+    bool isSpecial = true,
+  }) {
+    final length = 20;
+    final letterLowerCase = "abcdefghijklmnopqrstuvwxyz";
+    final letterUpperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    final number = '0123456789';
+    final special = '@#%^*>\$@?/[]=+';
+
+    String chars = "";
+    if (letter) chars += '$letterLowerCase$letterUpperCase';
+    if (isNumber) chars += '$number';
+    if (isSpecial) chars += '$special';
+
+    return List.generate(length, (index) {
+      final indexRandom = Random.secure().nextInt(chars.length);
+      return chars[indexRandom];
+    }).join('');
   }
 }
